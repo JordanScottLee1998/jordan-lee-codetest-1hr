805cafa33fd37fc0954d7a182b1f63b3e4405828
JordanLee1998 <jordanscottlee.1998@gmail.com>
2022-08-06 23:47:52 +0100

    Adding JS for API back to project

--- fe-codetest-main/src/js/main.js
+++ fe-codetest-main/src/js/main.js
@@ -1,1 +1,17 @@
 console.log("javascript entry point");
+
+$(document).ready(function(){
+    console.log("Didn't have time to insert these as articles unfortunately!")
+    getData();
+})
+
+function getData(){
+    $.ajax({
+        url: 'https://codetest.drumbeat-server.co.uk/wp-json/wp/v2/posts?_embed&per_page=100',
+        async:false,
+        crossDomain: true,
+        success: function(data) {
+            console.log(data);
+        }
+    });
+}
